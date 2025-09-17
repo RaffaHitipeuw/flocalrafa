ecb3fa4abf28560cbec62e6231256d28480e0061
Raffa <203495392+RaffaHitipeuw@users.noreply.github.com>
2025-09-17 11:23:12 +0700

    Second Summit

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,11 +1,11 @@
 "use client";
+
 import { motion } from "framer-motion";
-import { useRef } from "react";
 import styles from "./style.module.scss";
 import { slideUp } from "./animation";
+import Dither from "./Dither";
 
 export default function LandingPage() {
-
   return (
     <motion.main
       variants={slideUp}
@@ -13,6 +13,18 @@ export default function LandingPage() {
       animate="enter"
       className={styles.landing}
     >
+      <div style={{ width: '100%', height: '900px', position: 'relative' }}>
+        <Dither
+          waveColor={[0.5, 0.5, 0.5]}
+          disableAnimation={false}
+          enableMouseInteraction={true}
+          mouseRadius={0.3}
+          colorNum={4}
+          waveAmplitude={0.3}
+          waveFrequency={3}
+          waveSpeed={0.05}
+        />
+      </div>
     </motion.main>
   );
 }
